1ad41006d82c72769597eb3c8baff99b6270cb39
royliu897 <royliu2007@gmail.com>
2025-11-12 22:52:29 -0600

    add validation next

--- process_data.py
+++ process_data.py
@@ -93,3 +93,6 @@ class PreprocessedScatteringDataset:
         torch.save(y_tensor, save_y)
         print(f"Saved {save_x} and {save_y} ({x_tensor.shape[0]} samples)")
 
+if __name__ == "__main__":
+    raw_dir = os.path.join(os.environ["WORK"], "data_generate", "data/nips_prelim/volume3")
+    processed_dir = os.path.join(os.environ["WORK"], "data_generate", "processed_nips_prelim/volume3")
